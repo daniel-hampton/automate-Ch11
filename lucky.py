a0388f67efc19753abbb1e19580fb7799f2d9417
Dan Hampton <dhampton084@gmail.com>
2017-04-04 17:39:45 -0400

    created downloadXkCD.py, lucky.py and selenium test.py

---
+++ lucky.py
@@ -0,0 +1,21 @@
+#! python3
+# lucky.py - Opens several Google search results
+
+import requests
+import sys
+import webbrowser
+import bs4
+
+
+print('Googling...') # display text while downloading the Google page
+res = requests.get('https://google.com/search?q=' + ' '.join(sys.argv[1:]))
+res.raise_for_status()
+
+# retrieve top search results links
+soup = bs4.BeautifulSoup(res.text, 'lxml')
+
+# Open a browser tab for each result.
+linkElems = soup.select('.r a')
+numOpen = min(5, len(linkElems))
+for i in range(numOpen):
+    webbrowser.open('https://google.com/' + linkElems[i].get('href'))
